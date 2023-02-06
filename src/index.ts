7a92573fdaeae93c616d3e7d22477c01626a8156
lurong <lurongv@qq.com>
2023-02-06 19:20:42 +0800

    fix: some bug

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,15 @@ import { create, UseBoundStore, StoreApi } from "zustand"
 
 type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y ? 1 : 2 ? true : false
 
-type GetWritableKeys<T, K = keyof T> = K extends keyof T ? (Equal<Pick<T, K>, Readonly<Pick<T, K>>> extends true ? never : K ) : never
+type GetWritableKeys<T, K = keyof T> = K extends keyof T ? (Equal<Pick<T, K>, Readonly<Pick<T, K>>> extends true ? never : K) : never
 
 type GetWritableState<T> = {
     [K in GetWritableKeys<T>]: T[K]
 }
 
-type GetSetState<T extends Object> = (newState: Partial<GetWritableState<T>> | ((oldState: T) => Partial<GetWritableState<T>>)) => void
+type GetSetState<T extends Object, P extends Object = {}> = (newState: Partial<GetWritableState<T>> | ((oldState: T & P) => Partial<GetWritableState<T>>)) => void
 
-type GetStore<T extends Object, P extends Object = {}> = T & { setState: GetSetState<T> } & P
+type GetStore<T extends Object, P extends Object = {}> = T & { setState: GetSetState<T, P> } & P
 
 function createStore<T extends Object>(state: T): UseBoundStore<StoreApi<GetStore<T>>>
 
