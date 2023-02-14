af7e195109e10b3b6b1f3aa9f99b2fcdc30218e7
lurong <lurongv@qq.com>
2023-02-14 09:14:02 +0800

    feat: add export

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,11 +1,11 @@
 import { StoreApi } from "zustand";
-type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y ? 1 : 2 ? true : false;
-type GetWritableKeys<T, K = keyof T> = K extends keyof T ? (Equal<Pick<T, K>, Readonly<Pick<T, K>>> extends true ? never : K) : never;
-type GetWritableState<T> = {
+export type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y ? 1 : 2 ? true : false;
+export type GetWritableKeys<T, K = keyof T> = K extends keyof T ? (Equal<Pick<T, K>, Readonly<Pick<T, K>>> extends true ? never : K) : never;
+export type GetWritableState<T> = {
     [K in GetWritableKeys<T>]: T[K];
 };
-type GetStore<T extends Object, P extends Object = {}> = T & P;
-interface GetReturn<T extends Object, P extends Object = {}> {
+export type GetStore<T extends Object, P extends Object = {}> = T & P;
+export interface GetReturn<T extends Object, P extends Object = {}> {
     (): [GetStore<T, P>, StoreApi<GetStore<GetWritableState<T>>>["setState"], StoreApi<GetStore<T, P>>["subscribe"]];
     getState: StoreApi<GetStore<T, P>>["getState"];
     setState: StoreApi<GetStore<GetWritableState<T>>>["setState"];
