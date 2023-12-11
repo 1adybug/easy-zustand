d61063b791f8ad4a5f059c8895d4037b37f0af0c
luzixu <lurongv@qq.com>
2023-12-11 11:19:04 +0800

    新增 createPersistentSignalStore 方法

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import { isPlainObject } from "lodash-es"
 import { StoreApi, UseBoundStore, create } from "zustand"
 import { persist, createJSONStorage, PersistOptions } from "zustand/middleware"
+export { isPlainObject } from "lodash-es"
 
 export interface StateStorage {
     getItem: (name: string) => string | null | Promise<string | null>
@@ -34,7 +36,7 @@ function createStore<T>(state: T, replace: false): UseStore<T, false>
 function createStore<T>(state: T, replace: true): UseStore<T, true>
 
 function createStore<T, P extends boolean>(state: T, replace?: P): UseStore<T, P> {
-    replace ??= !(state !== undefined && state !== null && Object.getPrototypeOf(state) === Object.prototype) as P
+    replace ??= !isPlainObject(state) as P
     const originUseStore = create(() => state)
     let setState: UseStore<T, P>["setState"]
 
@@ -61,17 +63,23 @@ function createStore<T, P extends boolean>(state: T, replace?: P): UseStore<T, P
 }
 
 interface CreatePersistentStoreOption<State, T extends boolean | undefined = false> {
-    /** 用来标识状态的唯一 id */
+    /** 这是唯一必需的选项。给定的名称将是用于在存储中存储 Zustand 状态的密钥，因此它必须是唯一的 */
     name: string
     /** 改变状态是否完全覆盖，默认为 false */
     replace?: T
     /** 用来持久化的存储，默认是 localStorage */
     storage?: StateStorage | (() => StateStorage)
+    /** 选择存储哪些字段 */
     partialize?: (state: State) => Partial<State>
+    /** 用于在重新加载状态时调用的回调 */
     onRehydrateStorage?: (state: State) => ((state?: State, error?: Error) => void) | void
+    /** 如果您想在存储中引入重大更改（例如重命名字段），您可以指定新的版本号。默认情况下，如果存储中的版本与代码中的版本不匹配，则不会使用存储的值。您可以使用迁移功能（见下文）来处理重大更改，以便保留以前存储的数据 */
     version?: number
+    /** 您可以使用此选项来处理版本迁移。 migrate 函数将持久状态和版本号作为参数。它必须返回符合最新版本（代码中的版本）的状态。 */
     migrate?: (persistedState: State, version: number) => State | Promise<State>
+    /** 在某些情况下，您可能希望使用自定义合并函数将持久值与当前状态合并。 */
     merge?: (persistedState: State, currentState: State) => State
+    /** 默认情况下，存储将在初始化时被水化。 */
     skipHydration?: boolean
 }
 
@@ -103,7 +111,7 @@ export function createPersistentStore<T extends object>(state: IsPlainObject<T> 
 export function createPersistentStore<T extends object>(state: IsPlainObject<T> extends true ? T : never, option: CreatePersistentStoreOption<T, false>): UsePersistentStore<T, false>
 export function createPersistentStore<T extends object>(state: IsPlainObject<T> extends true ? T : never, option: CreatePersistentStoreOption<T, true>): UsePersistentStore<T, true>
 export function createPersistentStore<T extends object, P extends boolean>(state: IsPlainObject<T> extends true ? T : never, nameOrOption: CreatePersistentStoreOption<T, P> | string) {
-    const defaultReplace = !(state !== undefined && state !== null && Object.getPrototypeOf(state) === Object.prototype)
+    const defaultReplace = !isPlainObject(state)
 
     if (defaultReplace) {
         throw new TypeError("`createPersistentStore` just supports plain object")
@@ -154,6 +162,68 @@ export function createPersistentStore<T extends object, P extends boolean>(state
     useStore.setState = setState
     useStore.getState = originUseStore.getState
     useStore.subscribe = originUseStore.subscribe
+    return useStore
+}
+
+interface CreatePersistentSignalStoreOption {
+    /** 这是唯一必需的选项。给定的名称将是用于在存储中存储 Zustand 状态的密钥，因此它必须是唯一的 */
+    name: string
+    /** 用来持久化的存储，默认是 localStorage */
+    storage?: StateStorage | (() => StateStorage)
+    /** 如果您想在存储中引入重大更改（例如重命名字段），您可以指定新的版本号。默认情况下，如果存储中的版本与代码中的版本不匹配，则不会使用存储的值。您可以使用迁移功能（见下文）来处理重大更改，以便保留以前存储的数据 */
+    version?: number
+}
+
+export interface UsePersistentSignalStore<T> {
+    (): [T, (state: T | ((prevState: T) => T)) => void]
+    getState(): T
+    setState(state: T | ((prevState: T) => T)): void
+    subscribe(listener: (state: T, prevState: T) => void): () => void
+}
+
+/** 创建非对象类型 */
+export function createPersistentSignalStore<T>(state: T, name: string): UsePersistentSignalStore<T>
+export function createPersistentSignalStore<T>(state: T, option: CreatePersistentSignalStoreOption): UsePersistentSignalStore<T>
+export function createPersistentSignalStore<T>(state: T, nameOrOption: CreatePersistentSignalStoreOption | string) {
+    const option: CreatePersistentSignalStoreOption = typeof nameOrOption === "string" ? { name: nameOrOption, storage: localStorage } : nameOrOption
+
+    const { name, storage, version } = option
+
+    const o = {
+        name,
+        storage: createJSONStorage(typeof storage === "function" ? storage : () => storage || localStorage),
+        version
+    }
+
+    const originUseStore = create(persist(() => ({ value: state }), o))
+
+    function getState() {
+        return originUseStore.getState().value
+    }
+
+    function setState(newState: T | ((prevState: T) => T)) {
+        const prevState = getState()
+        if (typeof newState !== "function") {
+            if (newState === prevState) return
+            return originUseStore.setState({ value: newState })
+        }
+        const nextState = (newState as (prevState: T) => T)(prevState)
+        if (nextState === prevState) return
+        originUseStore.setState({ value: nextState })
+    }
+
+    function subscribe(listener: (state: T, prevState: T) => void) {
+        return originUseStore.subscribe((state, prevState) => listener(state.value, prevState.value))
+    }
+
+    const useStore: UsePersistentSignalStore<T> = () => {
+        const { value: state } = originUseStore()
+        return [state, setState]
+    }
+
+    useStore.getState = getState
+    useStore.setState = setState
+    useStore.subscribe = subscribe
     return useStore
 }
 
